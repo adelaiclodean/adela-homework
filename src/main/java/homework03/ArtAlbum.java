2915685ecc3ad52987906149b88c3c08873ff5b7
iclod <adella.stefan@gmail.com>
2022-06-22 22:43:26 +0300

    Add homework03

---
+++ src/main/java/homework03/ArtAlbum.java
@@ -0,0 +1,16 @@
+package homework03;
+
+public class ArtAlbum extends Book {
+
+    private final String paperQuality;
+
+    public ArtAlbum(String name, int numeberOfpages, String paperQuality) {
+        super(name, numeberOfpages);
+        this.paperQuality = paperQuality;
+    }
+    //public void print(){
+    //System.out.println("listeaza artAlbum");
+    // }
+}
+
+
---
+++ src/main/java/homework03/Book.java
@@ -0,0 +1,22 @@
+package homework03;
+
+public class Book {
+    private final String name;
+    private final int numberOfpages;
+
+    public Book(String name, int numberOfpages) {
+        this.name = name;
+        this.numberOfpages = numberOfpages;
+    }
+
+    public int getNumberOfpages() {
+        return numberOfpages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void start() {
+    }
+}
