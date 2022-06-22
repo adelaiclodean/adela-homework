6b9a12e09f7033c1138b0edb69a55403f0fbda6a
iclod <adella.stefan@gmail.com>
2022-06-22 21:57:44 +0300

    Add homework02updated

---
+++ src/main/java/homework02update/BubbleSort.java
@@ -0,0 +1,21 @@
+package homework02update;
+
+public class BubbleSort {
+    public static void sort (SalesRepresentative[] salesReps){
+
+        boolean changesPerformed=true;
+
+        while (changesPerformed){
+            changesPerformed=false;
+            for (int i = 0; i <salesReps.length-1 ; i++) {
+                if(salesReps[i].getRevenue()>salesReps[i+1].getRevenue()){
+                    SalesRepresentative temp=salesReps[i];
+                    salesReps[i]=salesReps[i+1];
+                    salesReps[i+1]=temp;
+                    changesPerformed=true;
+                }
+            }
+        }
+
+    }
+}
